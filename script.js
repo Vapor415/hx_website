05134350874c2a1383bf76eca5df676c4f191c91
Vapor415 <josiekarlie232@gmail.com>
2025-02-11 20:41:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // 示例商品数据
     const products = [
-        { name: "推荐1", image: "green1_00000.png", description: "这是商品1的描述。" },
+        { name: "推荐1", image: "tuijian.png", description: "这是商品1的描述。" },
         { name: "推荐2", image: "product2.jpg", description: "这是商品2的描述。" },
         { name: "推荐3", image: "product3.jpg", description: "这是商品3的描述。" },
         // 添加更多商品数据
